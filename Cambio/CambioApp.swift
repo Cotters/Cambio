8dc74d2355104c7d9fb61b0eeda6a1aa5c138bf6
Cotters <j.r.a.cotterell@gmail.com>
2025-07-08 13:52:39 +0100

    init

---
+++ Cambio/CambioApp.swift
@@ -0,0 +1,17 @@
+//
+//  CambioApp.swift
+//  Cambio
+//
+//  Created by Josh C on 08/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct CambioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
